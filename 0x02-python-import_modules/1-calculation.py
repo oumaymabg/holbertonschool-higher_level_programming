2cd5c33c78a0e779e9c647337fdf1dba095d27da
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-09-06 14:58:44 +0100

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-a = 10
-b = 5
-somme = add(a, b)
-som = sub(a, b)
-somm = mul(a, b)
-s = div(a, b)
-print("{} + {} = {}".format(a, b, somme))
-print("{} - {} = {}".format(a, b, som))
-print("{} * {} = {}".format(a, b, somm))
-print("{} / {} = {}".format(a, b, s))
+    a = 10
+    b = 5
+    somme = add(a, b)
+    som = sub(a, b)
+    somm = mul(a, b)
+    s = div(a, b)
+    print("{} + {} = {}".format(a, b, somme))
+    print("{} - {} = {}".format(a, b, som))
+    print("{} * {} = {}".format(a, b, somm))
+    print("{} / {} = {}".format(a, b, s))
