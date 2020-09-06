a3902447065961811aea467ddb4124d03c300ccc
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-09-06 14:55:25 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-sum = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, sum))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    sum = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, sum))
