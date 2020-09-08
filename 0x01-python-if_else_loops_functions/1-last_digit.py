800f23a2cb9b202c8f9737955037f3f1f627253a
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-09-08 20:30:43 +0100

    Update 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-l = int(repr(number)[-1]
-    if (l > 5):
-        print("Last digit of {:d} is {:d} and is greater than 5".format(number, l))
-    if (l == 5):
-        print("Last digit of {:d} is {:d} and is 0".format(number, l))
-    if ((l < 6) and (number != 0)):
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number, l))
-    if (number>5)
-        print ("and is greater than 5")
-    if (number == 0)
-        print ("and is 0)
-    if (number < 6 )
-        print ("and is less than 6 and not 0")
+
+if (number < 0):
+    print('Last digit of {:d} '
+          'is {:d} and is less than 6 and not 0'
+          .format(number, -(-number % 10)))
+
+elif number % 10 > 5:
+    print('Last digit of {:d} '
+          'is {:d} and is greater than 5'.format(number, number % 10))
+elif number % 10 == 0:
+    print('Last digit of {:d} '
+          'is 0 and is 0'.format(number))
+else:
+    print('Last digit of {:d} '
+          'is {:d} and is less than 6 and not 0'.format(number, number % 10))
+    
