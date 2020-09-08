ca02bad0adc1feab3ea2e2d8079280cd476e2fb2
oumaymabg <oumaymabou257@gmail.com>
2020-09-08 10:39:04 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
-length = len(sys.argv) - 1
-if length == 0:
-    print('{} arguments.'.format(length))
-elif length == 1:
-    print('{} argument:'.format(length))
-    print('{}: {}'.format(length, sys.argv[1]))
-else:
-    print('{} arguments:'.format(length))
-    for n in range(1, len(sys.argv)):
-        print('{}: {}'.format(n, sys.argv[n]))
+if __name__ == '__main__':
+    length = len(sys.argv) - 1
+    if length == 0:
+        print('{} arguments.'.format(length))
+    elif length == 1:
+        print('{} argument:'.format(length))
+        print('{}: {}'.format(length, sys.argv[1]))
+    else:
+        print('{} arguments:'.format(length))
+        for n in range(1, len(sys.argv)):
+            print('{}: {}'.format(n, sys.argv[n]))
