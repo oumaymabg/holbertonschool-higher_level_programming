0e9b9cc330eac659bf11c3bee729da2df7ed32cc
oumaymabg <oumaymabou257@gmail.com>
2020-09-08 21:07:41 +0000

    loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('129'), ord('154')):
-    if ((chr(i) != '145') and (chr(i) != '133')):
-        print(chr(i))
+for i in range(97, 123):
+    if ((chr(i) != 'q') and (chr(i) != 'e')):
+        print("{}".format(chr(i)), end='')
