7c4ec4f2dd5b22c3c3f1b07546bd07d9cb185071
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-09-08 13:22:13 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,7 +6,7 @@ if __name__ == '__name':
         print('0')
     elif length == 1:
         print('{}: {}'.format(length, sys.argv[1]))
-    else length > 1
+    else length > 1:
         print('{} arguments:'.format(length))
         for n in range(1, len(sys.argv)):
             print('{}: {}'.format(n, sys.argv[n]))
