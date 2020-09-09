57ed614bc630ce356d7fc96377f04854fb742850
oumaymabg <oumaymabou257@gmail.com>
2020-09-09 13:15:34 +0000

    maj

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,25 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-<<<<<<< HEAD
-=======
-
->>>>>>> 43fcea7d944bd092fe5abea30255a1221c74091b
-if (number < 0):
-    print('Last digit of {:d} '
-          'is {:d} and is less than 6 and not 0'
-          .format(number, -(-number % 10)))
-
-elif number % 10 > 5:
-    print('Last digit of {:d} '
-          'is {:d} and is greater than 5'.format(number, number % 10))
-elif number % 10 == 0:
-    print('Last digit of {:d} '
-          'is 0 and is 0'.format(number))
-else:
-    print('Last digit of {:d} '
-          'is {:d} and is less than 6 and not 0'.format(number, number % 10))
-<<<<<<< HEAD
 l = int(repr(number)[-1]
     if (l > 5):
         print("Last digit of {:d} is {:d} and is greater than 5".format(number, l))
@@ -33,6 +14,3 @@ l = int(repr(number)[-1]
         print ("and is 0)
     if (number < 6 )
         print ("and is less than 6 and not 0")
-=======
-    
->>>>>>> 43fcea7d944bd092fe5abea30255a1221c74091b
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(00, 100):
-    print("{:02d},".format(i), end='')
+for i in range(99):
+    print("{:02d},".format(i), end=' ')
+print("99")
