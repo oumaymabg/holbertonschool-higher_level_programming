540754b31db2191648753178c0449cfc5abd5ab3
oumaymabg <oumaymabou257@gmail.com>
2020-09-10 10:54:09 +0000

    print_list_integer

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for n in range(0, len(my_list=[])):
-        print({: d}.str.format(n))
+    for n in range(len(my_list=[])):
+        print({: d},str.format(n))
