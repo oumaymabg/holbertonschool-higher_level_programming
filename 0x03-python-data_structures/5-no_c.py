f57803729c13b047395491dcb5ae3ec50aff2aac
oumaymabg <oumaymabou257@gmail.com>
2020-09-13 13:06:07 +0000

    c value remover

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    for n in range(my_string):
-        print(n)
+    for n in range(0, len(my_string)):
+        if(my_string[i] == 'c' or my_string[i] == 'C'):
+            my_string[i] = ''
+    return(my_string)
