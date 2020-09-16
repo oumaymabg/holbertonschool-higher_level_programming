7ed35a092e4b0e53befaacafa8b7570294010711
oumaymabg <oumaymabou257@gmail.com>
2020-09-16 21:56:46 +0000

    multiple_returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-        if not(sentence):
-        return(None)
+    if sentence:
+        new_t = ("len(sentence), not(sentence)
+    else:
+        new_t = (len(sentence), None)
+    return(new_t)
