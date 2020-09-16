4843b627062daa6e3f9d6b1919e37fbb952e4540
oumaymabg <oumaymabou257@gmail.com>
2020-09-16 21:43:50 +0000

    add_tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a < 2:
+    if len(tuple_a) < 2:
         return(0)
-    else tuple_a > 2:
+    if len(tuple_a) > 2:
         return(tuple_a[:2])
