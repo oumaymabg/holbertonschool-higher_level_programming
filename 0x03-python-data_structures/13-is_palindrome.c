aa38f9bec4559030a70994d254dc7126486b8b38
oumaymabg <oumaymabou257@gmail.com>
2020-09-17 10:16:42 +0000

    palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * is_palindrome - test list palindrom
+ * @head: pointer to list to be tested
+ * Return: int
+ */
+int is_palindrome(listint_t **head);
