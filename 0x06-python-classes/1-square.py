cc5116b2b1016b7103bfce4bb787212f824c158d
oumaymabg <oumaymabou257@gmail.com>
2020-09-23 09:59:28 +0000

    1_square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""second_class_square"""
+
+
+class Square:
+
+
+    """Private instance attribute:size
+    Instantiation with size (no type/value verification)"""
+    def __init__ (self, size):
+    """Private instance attribute: size"""     
+    
