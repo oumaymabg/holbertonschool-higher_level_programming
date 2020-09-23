ac6e6312e39fa2537790a07c756028167ff1e0d4
oumaymabg <oumaymabou257@gmail.com>
2020-09-23 10:10:44 +0000

    square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,11 +3,9 @@
 
 
 class Square:
-
-
     """Private instance attribute:size
     Instantiation with size (no type/value verification)"""
-    def __init__ (self, size):
-    """Private instance attribute: size"""
-    self.__size = size     
-    
+
+    def __init__(self, size):
+        """Private instance attribute: size"""
+        self.__size = size
