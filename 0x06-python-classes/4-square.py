75c8c30644dee2c00ae1b600ec39b43e38f5a07b
oumaymabg <oumaymabou257@gmail.com>
2020-09-25 23:29:09 +0000

    4-square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3#!/usr/bin/python3
+#!/usr/bin/python3
 """Class_Square_Num_4"""
 
 
@@ -8,8 +8,7 @@ class Square:
     def __init__(self, size=0):
         self.__size = size  # private attribute
 
-    """Public instance method"""
-    def area(self):
+    def area(self): 
         return self.__size * self.__size
 
     @property
@@ -18,9 +17,9 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """instantiation with optional"""
+        """set_size instantiation with optional"""
         if type(value) != int:
-            raise typeError("size must be an integer")
-        elif value < 0:
+            raise TypeError("size must be an integer")
+        if value < 0:
             raise ValueError("size must be >=0")
-        self.__size =value
+        self.__size = value
