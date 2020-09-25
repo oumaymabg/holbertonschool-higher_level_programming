967526dc2ecb49755e23159e065c6933243b197d
oumaymabg <oumaymabou257@gmail.com>
2020-09-25 23:30:17 +0000

    4-square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         self.__size = size  # private attribute
 
-    def area(self): 
+    def area(self):
         return self.__size * self.__size
 
     @property
