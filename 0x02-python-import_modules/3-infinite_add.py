c2f98e7534ed185f01090182c0a4bff49ef9ab46
oumaymabg <oumaymabou257@gmail.com>
2020-09-26 20:27:02 +0000

    infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import sys
-if __name__ == '__name__':
-    length = len(sys.argv) - 1
-    if length == 0:
-        print('0')
-    elif length == 2x:
-        for n in range(1, len(sys.argv)):
-            print('{}: {}'.format(n, sys.argv[n]))
+from sys import argv
+
+if __name__ == "__main__":
+    length = len(argv)
+    sum = 0
+    for i in range(1, length):
+        sum += int(argv[i])
+    print("{:d}".format(sum))
