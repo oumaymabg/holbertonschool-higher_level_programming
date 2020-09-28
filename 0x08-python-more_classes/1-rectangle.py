45c654bf6a9b11451c94d1468c3bca46216b5d1f
oumaymabg <oumaymabou257@gmail.com>
2020-09-28 21:11:11 +0000

    1-rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""class_Real_definition_of_a_rectangle"""
+
+
+class Rectangle:
+    """Private instance attribute:width"""
+    def __init__(self, width=0, height=0):
+        """Instantiation with optional width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width  # private attribute
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    """Private instance attribute:height"""
+
+    @property
+    def height(self):
+        return self.__height  # private attribute
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
