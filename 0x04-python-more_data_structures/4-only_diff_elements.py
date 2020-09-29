3cc4d5430cd52031e193682ceb0e9516ccd60381
oumaymabg <oumaymabou257@gmail.com>
2020-09-29 21:50:15 +0000

    only-diff-elements

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
+    element = []
+    for n in set_1:
+        if n not in set_2:
+            element.append(n)
+    for n in set_2:
+        if n not in set_1:
+            element.append(n)
+    return element
