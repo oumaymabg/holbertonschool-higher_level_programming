3fd734e4ad1ccd0289f14fb1f00ec6086b31d580
oumaymabg <oumaymabou257@gmail.com>
2020-10-06 22:17:54 +0000

    6-base-geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""second_ class_BaseGeometry"""
+
+
+class BaseGeometry:
+    """Public instance method"""
+
+    def area(self):
+        """that raises an Exception with the message"""
+        raise Exception("area() is not implemented")
