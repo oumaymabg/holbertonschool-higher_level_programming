7602969744e6cb00b5ade5f4929fe953e58a2c94
oumaymabg <oumaymabou257@gmail.com>
2020-10-07 21:31:26 +0000

    json_string

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""task-5:To JSON string"""
+
+
+def to_json_string(my_obj):
+    """JSON:s a syntax for storing and exchanging data
+    my_obj: object
+    Write a function that returns the JSON representation
+    of an object (string)
+    """
+    return json.dumps(my_obj)
