7af2ba99082a54bea932e471dc2e22b22eadb064
oumaymabg <oumaymabou257@gmail.com>
2020-10-07 20:38:59 +0000

    read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""task_0:Read_file"""
+
+
+def read_file(filename=""):
+    """read_file:a function that reads a text file (UTF8)
+    and prints it to stdout
+    """
+    with open(filename, encoding="UTF-8") as file:
+        print(file.read(), end="")
