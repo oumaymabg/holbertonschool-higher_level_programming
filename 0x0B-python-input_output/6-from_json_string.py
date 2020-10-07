44f8e76f381186d69df15dfa136f42e3c43bb5fd
oumaymabg <oumaymabou257@gmail.com>
2020-10-07 21:38:21 +0000

    json_string

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""task-6:From_JSON_string_to_object"""
+import json
+
+
+def from_json_string(my_str):
+    """from_json_string: function that returns an object
+    represented by a JSON string
+    """
+    return json.dumps(my_str)
