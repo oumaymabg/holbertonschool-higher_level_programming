c6012ee43b74a9435ca287c3d35e2eba3188d6fd
oumaymabg <oumaymabou257@gmail.com>
2020-10-07 22:51:20 +0000

    pascal_triangle

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""task-14:Pascal's Triangle """
+
+
+def pascal_triangle(n):
+    """pascal_triangle:that returns a list of lists of integers
+    representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        pass
