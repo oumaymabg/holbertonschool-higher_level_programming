5e5e4a30f935539e30c0e98ddd419a55c1eae00d
oumaymabg <oumaymabou257@gmail.com>
2020-10-07 22:30:29 +0000

    student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,16 +6,16 @@ class student:
     """Public instance attributes"""
 
 
-def __init__(self, first_name, last_name, age):
-    """Instantiation with first_name, last_name and age"""
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
-"""Public method"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    """Public method"""
 
 
-def to_json(self):
-    """that retrieves a dictionary representation of
-    a Student instance
-    """
-    return self.__dict__
+    def to_json(self):
+        """that retrieves a dictionary representation of
+        a Student instance
+        """
+        return self.__dict__
