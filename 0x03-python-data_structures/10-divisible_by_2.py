ec736e698a978b30757a1c08fbca116861a30849
oumaymabg <oumaymabou257@gmail.com>
2020-12-05 13:07:52 +0000

    divisible_by_2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    if my_list:
-        return(max(my_list, key=my_list.get))
+def best_score(a_dictionary):
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get)
