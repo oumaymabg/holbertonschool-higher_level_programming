66b9f750326ce0869db43b010e517ecab04f820d
oumaymabg <oumaymabou257@gmail.com>
2021-01-11 20:28:13 +0000

    delet.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a DELETE request to URL and displays the body of the response
-curl-sLX DELETE$1
+# task2 :Sends a DELETE request to URL and displays the body of the response
+curl -s -L -X DELETE "$1"
