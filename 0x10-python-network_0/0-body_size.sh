e2ee908a90f04ebce153372a065471be3b4697ac
oumaymabg <oumaymabou257@gmail.com>
2021-01-11 15:15:59 +0000

    body-size_curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes a URL, sends a request, and returns the size of the body
-curl-w'%{size_download}\n'-so/dev/null$1
+#task 0 : Takes a URL, sends a request, and returns the size of the body
+curl -s "$1" | wc -c
