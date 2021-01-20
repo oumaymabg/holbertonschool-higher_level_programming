9c6c2b512d132a9b14436daf6f871aebdce695f1
oumaymabg <oumaymabou257@gmail.com>
2021-01-20 14:40:44 +0000

    methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays all HTTP methods the server will accept
-curl-sI"$1" | grep Allow | cut-d ' '-f2-
+# task3: Displays all HTTP methods the server will accept
+curl -sI "$1" | grep Allow | cut -d ' ' -f2-
