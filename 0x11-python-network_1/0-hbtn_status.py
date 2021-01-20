710b90c230913c97f7d963a1adf770d91e1cd06a
oumaymabg <oumaymabou257@gmail.com>
2021-01-20 15:52:45 +0000

    hbtn_status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,8 @@
 """ task0 :Write a Python script that fetches """
 
 import urllib.request
-if __name__ == "__main__":
+
+
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
     print('Body response:')
