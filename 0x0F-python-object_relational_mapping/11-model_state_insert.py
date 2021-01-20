d32718ea93e7ca3c5e1000dd9e351acf77a7f1cd
oumaymabg <oumaymabou257@gmail.com>
2021-01-20 13:49:57 +0000

    model_state_insert

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""  Adds data to a table """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = State(name="Louisiana")
+    session.add(state)
+    session.commit()
+    print(state.id)
+    session.close()
