07bcd73ab36986d945837c947de6b23e7360bc68
oumaymabg <oumaymabou257@gmail.com>
2021-01-20 12:21:01 +0000

    filter_states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-# lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
-# traitement: ./1-filter_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+# lists all states with a name starting with N
 import MySQLdb
 from sys import argv
 
