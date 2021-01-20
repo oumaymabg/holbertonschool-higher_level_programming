be1fa9e284627e1693b24a7f97a067d32e65c843
oumaymabg <oumaymabou257@gmail.com>
2021-01-20 13:47:16 +0000

    model_stete_my_get

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" SQLAlchemy  from table """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(
+            State.name == argv[4]).first()
+    if state is None:
+        print("Not found")
+    else:
+        print(state.id)
+    session.close()
