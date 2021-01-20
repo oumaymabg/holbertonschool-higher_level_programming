f47b969651b4357a9e7bd55279ec950ad531a49e
oumaymabg <oumaymabou257@gmail.com>
2021-01-20 12:30:11 +0000

    filter-states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,15 +3,16 @@
 import MySQLdb
 from sys import argv
 
-if name == "main":
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY\
-              'N%' ORDER BY states.id")
-    [print(state) for state in cursor.fetchall()]
-cursor.close()
-db.close()
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT * from states WHERE name LIKE '{}' COLLATE latin1_general_cs\
+                ORDER BY states.id".format(argv[4]))
+    for state in c.fetchall():
+        print(state)
+    c.close()
+    db.close()
