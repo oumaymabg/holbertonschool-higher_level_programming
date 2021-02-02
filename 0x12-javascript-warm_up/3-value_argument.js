8eceaded36c0883dc91a5b0ece8115e71d0d2dbf
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2021-02-02 16:14:14 +0100

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 // Value of my argument
 
 if (process.argv[2] === undefined) {
-    console.log('No argument');
+  console.log('No argument');
 } else {
-    console.log(process.argv[2]);
+  console.log(process.argv[2]);
 }
