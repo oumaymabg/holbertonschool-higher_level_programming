e0d744e5b6e39922dea1165db2503c7d30b3c07d
oumaymabg <oumaymabou257@gmail.com>
2021-02-09 12:44:19 +0000

    starwars_title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/';
+const url = 'https://swapi-api.hbtn.io/api/films/:id';
 const episode = process.argv[2];
 request(url + episode, function (err, response, body) {
   if (err) {
